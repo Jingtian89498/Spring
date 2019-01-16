dc1ed525d04ba2ea62728faddf92311f5abee263
ljtian <ljt89498@163.com>
2019-01-16 14:25:04 +0800

    Pointcut

--- src/com/ljt/aop/Audience.java
+++ src/com/ljt/aop/Audience.java
@@ -2,13 +2,25 @@ package com.ljt.aop;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
 
 @Aspect
 public class Audience {
 
+    @Pointcut("execution(* com.ljt.aop.Performance.perform(..))")
+    public void performance(){
+
+    }
+
     @Before("execution(* com.ljt.aop.Performance.perform(..))")
     public void silenceCellPhone(){
         System.out.println("Silencing cell phones");
+    }
+
+    @Before("performance()")
+    public void takeSeats(){
+        System.out.println("Taking seats");
     }
 
 }
